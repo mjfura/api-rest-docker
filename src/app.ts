d13799ff7d04b35d8c6e0d9e754ee5f093356043
marco <marco.fura@unmsm.edu.pe>
2022-12-11 10:06:51 -0500

    feat(server): add / endpoint to server

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+const app=express()
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json())
+
+app.use("/",(_req,res)=>{
+    return res.status(200).send({
+        message:"API REST using Docker Compose"
+    })
+})
+
+export default app

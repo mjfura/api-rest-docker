66922284f2315ce04f9718262f8b50aa524576cc
marco <marco.fura@unmsm.edu.pe>
2022-12-11 23:20:55 -0500

    feat(docker): add production environment

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:18-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+COPY pnpm-lock.yaml ./
+
+RUN npm install -g pnpm
+RUN pnpm install
+
+COPY . .
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,23 @@ app.listen(PORT,async ()=>{
     try{
         if(!MONGO_URI)throw new Error("No se ha encontrado la URI de la base de datos")
         mongoose.connect(MONGO_URI,(err)=>{
-            if(err) throw err
+            if(err){
+                console.log("PORT",PORT)
+                console.log("MONGO_URI",MONGO_URI)
+                console.log("NODE_ENV",NODE_ENV)
+                console.log("error al intentar conectarse a la base de datos",err)
+                return
+            }
             console.log("Base de datos conectada")
         })
+        console.log("PORT",PORT)
+                console.log("MONGO_URI",MONGO_URI)
+                console.log("NODE_ENV",NODE_ENV)
         console.log(`Servidor corriendo en el puerto: ${PORT}!!  en un entorno de ${NODE_ENV}`)
     }catch(e){
+        console.log("PORT",PORT)
+        console.log("MONGO_URI",MONGO_URI)
+        console.log("NODE_ENV",NODE_ENV)
         console.log("error al intentar conectarse a la base de datos",e)
     }
 }

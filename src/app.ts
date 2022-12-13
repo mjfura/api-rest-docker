70f49a41b6fa4bbaf1654b1148ed5116ea20c420
marco <marco.fura@unmsm.edu.pe>
2022-12-12 19:26:31 -0500

    fix(app): remove / endpoint

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,11 @@ import cors from "cors"
 import usersRouter from "./features/users/routes"
 import {config}from "dotenv"
 config()
-const NODE_ENV=process.env.NODE_ENV
 const app=express()
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
-app.use("/",(_req,res)=>{
-    return res.status(200).send({
-        message:`API REST with MongoDB and NodeJS running in ${NODE_ENV}`
-    })
-})
 app.use("/api/users",usersRouter)
 
 export default app
